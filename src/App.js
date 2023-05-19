57c5c4dd319ae034642330162ca5d4282372d42b
Regis066 <mugisharegis72@gmail.com>
2023-05-19 22:26:38 +0200

    added some styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './App.css';
 const CarSellAppForm = () => {
   const [formData, setFormData] = useState({
     carMake: '',
@@ -32,180 +32,183 @@ const CarSellAppForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="container">
+    <form onSubmit={handleSubmit} className='form'>
+    <label>
+      Car Make:
+      <input
+        type="text"
+        name="carMake"
+        value={formData.carMake}
+        onChange={handleInputChange}
+        required
+      />
+    </label>
+    <label>
+      Car Model:
+      <input
+        type="text"
+        name="carModel"
+        value={formData.carModel}
+        onChange={handleInputChange}
+        required
+      />
+    </label>
+    <label>
+      Year:
+      <input
+        type="date"
+        name="year"
+        value={formData.year}
+        onChange={handleInputChange}
+        required
+      />
+    </label>
+    <label>
+      Mileage:
+      <input
+        type="number"
+        name="mileage"
+        value={formData.mileage}
+        onChange={handleInputChange}
+        required
+      />
+    </label>
+    <fieldset>
+      <legend>Condition:</legend>
       <label>
-        Car Make:
+        Excellent
         <input
-          type="text"
-          name="carMake"
-          value={formData.carMake}
+          type="radio"
+          name="condition"
+          value="Excellent"
+          checked={formData.condition === 'Excellent'}
           onChange={handleInputChange}
           required
         />
       </label>
       <label>
-        Car Model:
+        Good
         <input
-          type="text"
-          name="carModel"
-          value={formData.carModel}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <label>
-        Year:
-        <input
-          type="date"
-          name="year"
-          value={formData.year}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <label>
-        Mileage:
-        <input
-          type="number"
-          name="mileage"
-          value={formData.mileage}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <fieldset>
-        <legend>Condition:</legend>
-        <label>
-          Excellent
-          <input
-            type="radio"
-            name="condition"
-            value="Excellent"
-            checked={formData.condition === 'Excellent'}
-            onChange={handleInputChange}
-            required
-          />
-        </label>
-        <label>
-          Good
-          <input
-            type="radio"
-            name="condition"
-            value="Good"
-            checked={formData.condition === 'Good'}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Fair
-          <input
-            type="radio"
-            name="condition"
-            value="Fair"
-            checked={formData.condition === 'Fair'}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Poor
-          <input
-            type="radio"
-            name="condition"
-            value="Poor"
-            checked={formData.condition === 'Poor'}
-            onChange={handleInputChange}
-          />
-        </label>
-      </fieldset>
-      <fieldset>
-        <legend>Features:</legend>
-        <label>
-          Air Conditioning
-          <input
-            type="checkbox"
-            name="features"
-            value="Air Conditioning"
-            checked={formData.features.includes('Air Conditioning')}
-            onChange={handleCheckboxChange}
-          />
-        </label>
-        <label>
-          Power Steering
-          <input
-            type="checkbox"
-            name="features"
-            value="Power Steering"
-            checked={formData.features.includes('Power Steering')}
-            onChange={handleCheckboxChange}
-          />
-        </label>
-        <label>
-          Power Windows
-          <input
-            type="checkbox"
-            name="features"
-            value="Power Windows"
-            checked={formData.features.includes('Power Windows')}
-            onChange={handleCheckboxChange}
-          />
-        </label>
-        <label>
-          ABS
-          <input
-            type="checkbox"
-            name="features"
-            value="ABS"
-            checked={formData.features.includes('ABS')}
-            onChange={handleCheckboxChange}
-          />
-        </label>
-        <label>
-          Navigation System
-          <input
-            type="checkbox"
-            name="features"
-            value="Navigation System"
-            checked={formData.features.includes('Navigation System')}
-            onChange={handleCheckboxChange}
-          />
-        </label>
-      </fieldset>
-      <label>
-        Transmission:
-        <select
-          name="transmission"
-          value={formData.transmission}
-          onChange={handleInputChange}
-          required
-        >
-          <option value="">Select</option>
-          <option value="Automatic">Automatic</option>
-          <option value="Manual">Manual</option>
-        </select>
-      </label>
-      <label>
-        Price Range:
-        <input
-          type="range"
-          name="priceRange"
-          min="0"
-          max="100"
-          value={formData.priceRange}
+          type="radio"
+          name="condition"
+          value="Good"
+          checked={formData.condition === 'Good'}
           onChange={handleInputChange}
         />
       </label>
       <label>
-        Contact Number:
+        Fair
         <input
-          type="text"
-          name="contactNumber"
-          value={formData.contactNumber}
+          type="radio"
+          name="condition"
+          value="Fair"
+          checked={formData.condition === 'Fair'}
           onChange={handleInputChange}
-          required
         />
       </label>
-      <button type="submit">Submit</button>
-    </form>
+      <label>
+        Poor
+        <input
+          type="radio"
+          name="condition"
+          value="Poor"
+          checked={formData.condition === 'Poor'}
+          onChange={handleInputChange}
+        />
+      </label>
+    </fieldset>
+    <fieldset>
+      <legend>Features:</legend>
+      <label>
+        Air Conditioning
+        <input
+          type="checkbox"
+          name="features"
+          value="Air Conditioning"
+          checked={formData.features.includes('Air Conditioning')}
+          onChange={handleCheckboxChange}
+        />
+      </label>
+      <label>
+        Power Steering
+        <input
+          type="checkbox"
+          name="features"
+          value="Power Steering"
+          checked={formData.features.includes('Power Steering')}
+          onChange={handleCheckboxChange}
+        />
+      </label>
+      <label>
+        Power Windows
+        <input
+          type="checkbox"
+          name="features"
+          value="Power Windows"
+          checked={formData.features.includes('Power Windows')}
+          onChange={handleCheckboxChange}
+        />
+      </label>
+      <label>
+        ABS
+        <input
+          type="checkbox"
+          name="features"
+          value="ABS"
+          checked={formData.features.includes('ABS')}
+          onChange={handleCheckboxChange}
+        />
+      </label>
+      <label>
+        Navigation System
+        <input
+          type="checkbox"
+          name="features"
+          value="Navigation System"
+          checked={formData.features.includes('Navigation System')}
+          onChange={handleCheckboxChange}
+        />
+      </label>
+    </fieldset>
+    <label>
+      Transmission:
+      <select
+        name="transmission"
+        value={formData.transmission}
+        onChange={handleInputChange}
+        required
+      >
+        <option value="">Select</option>
+        <option value="Automatic">Automatic</option>
+        <option value="Manual">Manual</option>
+      </select>
+    </label>
+    <label>
+      Price Range:
+      <input
+        type="range"
+        name="priceRange"
+        min="0"
+        max="100"
+        value={formData.priceRange}
+        onChange={handleInputChange}
+      />
+    </label>
+    <label>
+      Contact Number:
+      <input
+        type="text"
+        name="contactNumber"
+        value={formData.contactNumber}
+        onChange={handleInputChange}
+        required
+      />
+    </label>
+    <button type="submit" className='submit'>Submit</button>
+  </form>
+    </div>
+  
   );
 };
 
